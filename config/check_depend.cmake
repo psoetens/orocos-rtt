019d2ef1ea767f3d3244275a3b7883ab8e80ee10
Peter Soetens <peter@thesourceworks.com>
2009-10-02 15:03:40 +0000

    Merged revisions 30541,30545-30548,30616 via svnmerge from https://svn.mech.kuleuven.be/repos/orocos/trunk/rtt
    ........
      r30541 | psoetens | 2009-09-15 13:21:56 +0200 (Tue, 15 Sep 2009) | 4 lines
      
      cmake: fix generation of .pc files for include dirs and user link libraries with CORBA.
      (cherry picked from commit 3b1d51e7c155c10eddb5cd7a905a293dfcca502f)
      
      From: Peter Soetens <peter@thesourceworks.com>
    ........
      r30545 | psoetens | 2009-09-15 16:46:52 +0200 (Tue, 15 Sep 2009) | 3 lines
      
      cmake: fix .pc file for xenomai.
      
      From: Peter Soetens <peter@thesourceworks.com>
    ........
      r30546 | psoetens | 2009-09-15 16:46:54 +0200 (Tue, 15 Sep 2009) | 3 lines
      
      debian: fix build deps for boost in control file.
      
      From: Peter Soetens <peter@thesourceworks.com>
    ........
      r30547 | psoetens | 2009-09-15 16:46:57 +0200 (Tue, 15 Sep 2009) | 3 lines
      
      do not build locked container tests for boost < 1.36
      
      From: Peter Soetens <peter@thesourceworks.com>
    ........
      r30548 | psoetens | 2009-09-15 16:46:59 +0200 (Tue, 15 Sep 2009) | 4 lines
      
      tests: Fix boost version test.
      (cherry picked from commit 088aa53e044cde9c99e6f477de0d99433aad036a)
      
      From: Peter Soetens <peter@thesourceworks.com>
    ........
      r30616 | psoetens | 2009-10-02 16:57:07 +0200 (Fri, 02 Oct 2009) | 3 lines
      
      Only define _XOPEN_SOURCE if not set yet.
      
      From: Peter Soetens <peter@thesourceworks.com>
    ........
    
    
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/branches/rtt/rtt-1.10@30617 ce417995-dfc9-0310-95a0-acdaff106893

--- config/check_depend.cmake
+++ config/check_depend.cmake
@@ -102,6 +102,7 @@ if(OROCOS_TARGET STREQUAL "xenomai")
   find_package(Xenomai REQUIRED)
 
   if(XENOMAI_FOUND)
+    list(APPEND OROCOS-RTT_USER_LINK_LIBS native ) # For libraries used in inline (fosi/template) code.
     list(APPEND OROCOS-RTT_INCLUDE_DIRS ${XENOMAI_INCLUDE_DIRS} )
     list(APPEND OROCOS-RTT_LIBRARIES ${XENOMAI_LIBRARIES} pthread dl) 
     list(APPEND OROCOS-RTT_DEFINITIONS "OROCOS_TARGET=${OROCOS_TARGET}") 
@@ -257,6 +258,9 @@ if (ENABLE_CORBA)
 	    if( TAO_Messaging_FOUND )
 	      SET(CORBA_TAO_HAS_MESSAGING 1)
 	    endif()
+
+	    # Including a TAO header is sufficient to depend on this library.
+	    set(CORBA_USER_LINK_LIBS TAO_PortableServer)
 
         ENDIF(NOT TAO_FOUND)
     ELSEIF(${CORBA_IMPLEMENTATION} STREQUAL "OMNIORB")

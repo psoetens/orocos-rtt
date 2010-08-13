orocos-toolchain/rtt

win32: Export symbols

src/internal/ListLockFree.cpp
@@ -43,11 +43,11 @@
 #include "ListLockFree.hpp"
 
 
-void intrusive_ptr_add_ref( RTT::internal::IntrusiveStorage* p ) {
+void RTT_API intrusive_ptr_add_ref( RTT::internal::IntrusiveStorage* p ) {
     oro_atomic_inc( &p->ref );
 }
 
-void intrusive_ptr_release( RTT::internal::IntrusiveStorage* p ) {
+void RTT_API intrusive_ptr_release( RTT::internal::IntrusiveStorage* p ) {
     if (oro_atomic_dec_and_test( &p->ref ) )
         delete p;
 }

src/internal/ListLockFree.hpp
@@ -64,8 +64,8 @@ namespace RTT
 }
 
 
-void intrusive_ptr_add_ref(RTT::internal::IntrusiveStorage* p );
-void intrusive_ptr_release(RTT::internal::IntrusiveStorage* p );
+void RTT_API intrusive_ptr_add_ref(RTT::internal::IntrusiveStorage* p );
+void RTT_API intrusive_ptr_release(RTT::internal::IntrusiveStorage* p );
 
 namespace RTT
 { namespace internal {

psoetens/orocos-rtt

Signed-off-by: Peter Soetens <peter@thesourceworks.com>

src/ListLockFree.cpp
@@ -43,11 +43,11 @@
 #include "ListLockFree.hpp"
 
 
-void intrusive_ptr_add_ref( RTT::detail::IntrusiveStorage* p ) {
+void RTT_API intrusive_ptr_add_ref( RTT::detail::IntrusiveStorage* p ) {
     oro_atomic_inc( &p->ref );
 }
 
-void intrusive_ptr_release( RTT::detail::IntrusiveStorage* p ) {
+void RTT_API intrusive_ptr_release( RTT::detail::IntrusiveStorage* p ) {
     if (oro_atomic_dec_and_test( &p->ref ) )
         delete p;
 }

src/ListLockFree.hpp
@@ -64,8 +64,8 @@ namespace RTT
 }
 
 
-void intrusive_ptr_add_ref(RTT::detail::IntrusiveStorage* p );
-void intrusive_ptr_release(RTT::detail::IntrusiveStorage* p );
+void RTT_API intrusive_ptr_add_ref(RTT::detail::IntrusiveStorage* p );
+void RTT_API intrusive_ptr_release(RTT::detail::IntrusiveStorage* p );
 
 namespace RTT
 {